8be0fdd0e3c8a8ff247b7d81b8531dd134439a40
stefanFramework <cesar_cappetto@yahoo.com.ar>
2025-01-30 13:19:03 -0300

    Implementing Alembic configuration

---
+++ migrations/versions/e4564067b0be_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial Migration
+
+Revision ID: e4564067b0be
+Revises: 
+Create Date: 2025-01-30 16:17:07.938394
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e4564067b0be'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
